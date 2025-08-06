898cab0f3263274287c15c6d427f11bd56332e56
Milan <gorislavetsmilan321@gmail.com>
2025-08-06 18:12:32 +0500

    migrated to Nuxt 3.

---
+++ app/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class="text-center text-3xl font-bold">Hello from Nuxt3!</h1>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: ['@nuxt/fonts', '@nuxt/scripts', '@nuxt/image'],
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+})
