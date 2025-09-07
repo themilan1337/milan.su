3c9b72a6e972c25657edb9bb5e2221c588b410b0
Milan <76396495+themilan1337@users.noreply.github.com>
2025-09-08 00:50:14 +0500

    yeah

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
-    <h1 class="text-center text-3xl font-bold">Hello from Nuxt3!</h1>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ app/pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <main>
+        <div class="max-w-md mt-24 mx-auto flex flex-col">
+            <div class="header text-left text-zinc-700">
+                <img src="/milan.jpeg" class="rounded-full mb-4 h-16 w-16" alt="">
+                <h1 class="text-3xl font-bold serif">Milan Gorislavets.</h1>
+                <p class="text-zinc-600">
+                    #1 Peerlist Hunter and collaborator. <br> DM me if you want to work with me. CEO @ CatOps
+                </p>
+                <div class="flex space-x-4 my-8">
+                    <a href="#" class="text-blue-500 hover:text-blue-600 transition">
+                        <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                            <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path>
+                        </svg>
+                    </a>
+                    <a href="#" class="text-green-600 hover:text-green-700 transition">
+                        <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 15h-2v-6h2v6zm-1-7c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1zm5 7h-2v-3c0-.55-.45-1-1-1s-1 .45-1 1v3h-2v-6h2v1.1c.17-.31.48-.51.83-.51.83 0 1.5.67 1.5 1.5V17z"/>
+                        </svg>
+                    </a>
+                    <a href="#" class="text-zinc-800 hover:text-zinc-600 transition">
+                        <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                            <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"></path>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
