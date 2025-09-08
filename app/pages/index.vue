d892d8e0fe341ccaf779c85d80df7974cac9eb4c
Milan <76396495+themilan1337@users.noreply.github.com>
2025-09-08 12:21:21 +0500

    some stuff

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,25 +2,34 @@
     <main>
         <div class="max-w-md mt-24 px-4 mx-auto flex flex-col">
             <div class="header text-left text-zinc-700">
-                <img src="/milan.jpeg" class="rounded-full mb-4 h-16 w-16" alt="">
+                <div class="flex -space-x-6">
+                    <img src="/milan.jpeg" class="rounded-full mb-4 h-16 w-16 border-4 border-zinc-200" alt="">
+                    <img src="https://live-s3-bucket-6bjrdtnk7ph2.cdn.live.tandem.net/54/f1/371394bdf4698585fa89b0233f5e0dc1.jpg" class="rounded-full mb-4 h-16 w-16 border-4 border-zinc-200" alt="">
+                    <img src="https://live-s3-bucket-6bjrdtnk7ph2.cdn.live.tandem.net/16/d2/5c81075b598e5489ae0482bd303d8e75.jpg" class="rounded-full mb-4 h-16 w-16 border-4 border-zinc-200" alt="">
+                </div>
                 <h1 class="text-3xl font-bold serif">Milan Gorislavets.</h1>
                 <p class="text-zinc-600">
                     #1 Peerlist Hunter and collaborator. <br> DM me if you want to work with me. CEO @ CatOps
                 </p>
                 <div class="flex space-x-4 my-4">
-                    <a href="#" class="text-zinc-600 hover:text-zinc-600 transition">
+                    <a href="https://www.linkedin.com/in/gorislavets/" class="text-zinc-600 hover:text-zinc-400 transition">
                         <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><!-- Icon from Huge Icons by Hugeicons - undefined --><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" color="currentColor"><path d="M7 10v7m4-4v4m0-4a3 3 0 1 1 6 0v4m-6-4v-3M7.008 7h-.009"/><path d="M2.5 12c0-4.478 0-6.718 1.391-8.109S7.521 2.5 12 2.5c4.478 0 6.718 0 8.109 1.391S21.5 7.521 21.5 12c0 4.478 0 6.718-1.391 8.109S16.479 21.5 12 21.5c-4.478 0-6.718 0-8.109-1.391S2.5 16.479 2.5 12"/></g></svg>
                     </a>
-                    <a href="#" class="text-zinc-600 hover:text-zinc-600 transition">
+                    <a href="https://x.com/@MGorislavets" class="text-zinc-600 hover:text-zinc-400 transition">
                         <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><!-- Icon from Huge Icons by Hugeicons - undefined --><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" color="currentColor"><path d="M2.5 12c0-4.478 0-6.718 1.391-8.109S7.521 2.5 12.001 2.5c4.478 0 6.717 0 8.108 1.391S21.5 7.521 21.5 12c0 4.478 0 6.718-1.391 8.109S16.479 21.5 12 21.5c-4.478 0-6.717 0-8.109-1.391c-1.39-1.392-1.39-3.63-1.39-8.109"/><path d="m7 17l4.194-4.193M17 7l-4.193 4.194m0 0L9.777 7H7l4.194 5.807m1.613-1.614L17 17h-2.778l-3.028-4.193"/></g></svg>
                     </a>
-                    <a href="#" class="text-zinc-600 hover:text-zinc-600 transition">
+                    <a href="https://instagram.com/milan1337_" class="text-zinc-600 hover:text-zinc-400 transition">
                         <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><!-- Icon from Huge Icons by Hugeicons - undefined --><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" color="currentColor"><path d="M2.5 12c0-4.478 0-6.718 1.391-8.109S7.521 2.5 12 2.5c4.478 0 6.718 0 8.109 1.391S21.5 7.521 21.5 12c0 4.478 0 6.718-1.391 8.109S16.479 21.5 12 21.5c-4.478 0-6.718 0-8.109-1.391S2.5 16.479 2.5 12"/><path d="M16.5 12a4.5 4.5 0 1 1-9 0a4.5 4.5 0 0 1 9 0m1.008-5.5h-.01"/></g></svg>
                     </a>
-                    <a href="#" class="text-zinc-600 hover:text-zinc-600 transition">
+                    <a href="https://peerlist.io/goris" class="text-zinc-600 hover:text-zinc-400 transition">
                         <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><!-- Icon from Iconoir by Luca Burgio - https://github.com/iconoir-icons/iconoir/blob/main/LICENSE --><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"><path d="M8.87 3h6.26a6 6 0 0 1 5.963 5.337l.21 1.896c.131 1.174.131 2.36 0 3.534l-.21 1.896A6 6 0 0 1 15.13 21H8.87a6 6 0 0 1-5.963-5.337l-.21-1.896a16 16 0 0 1 0-3.534l.21-1.896A6 6 0 0 1 8.87 3"/><path d="M9 17v-4m0 0V7h4a3 3 0 0 1 3 3v0a3 3 0 0 1-3 3z"/></g></svg>
                     </a>
                 </div>
+                <p class="text-zinc-600">
+                        👋🏻 Hello, there! My name is Milan and im really into programming (especially web development). Also, i love creating amazing AI products.
+                        <br><br>
+                        When i was 11 i started learning how to code. That's funny because it was almost 6 years ago but im only 16 and a half right now.
+                </p>
             </div>
         </div>
     </main>
