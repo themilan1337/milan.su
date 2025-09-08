5c800833e80a75d2b3bc493d9eeffa46dd9b9270
Milan <76396495+themilan1337@users.noreply.github.com>
2025-09-08 14:35:10 +0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -8,10 +8,26 @@
                     <img src="https://live-s3-bucket-6bjrdtnk7ph2.cdn.live.tandem.net/16/d2/5c81075b598e5489ae0482bd303d8e75.jpg" class="rounded-full object-cover mb-4 h-16 w-16 border-4 border-zinc-200" alt="">
                 </div>
                 <h1 class="text-3xl font-bold serif">Milan Gorislavets.</h1>
-                <p class="text-zinc-600">
-                    🚀 #1 Peerlist Hunter | Collaborator | CEO @ CatOps <br>
-                    Open to exciting projects (DM if you’d like to work together)
-                </p>
+<p class="text-zinc-600 flex flex-col sm:flex-row items-start sm:items-center gap-2 flex-wrap">
+    <span class="flex items-center gap-2">
+        🚀 #1 Peerlist Hunter | Collaborator | CEO @ 
+        <NuxtLink 
+            to="https://catops.net/" 
+            class="hover:text-zinc-800 transition-colors inline-flex items-center"
+        >
+        <img 
+                src="https://media.licdn.com/dms/image/v2/D4E0BAQHJ_hiMESMFDQ/company-logo_100_100/B4EZhTwnvQGYAQ-/0/1753751930834/moniq_sh_logo?e=1759968000&v=beta&t=4oQcnGARN1uf61Kn-AqnVj-P4GPsb-tgCVwhO5TbbcY"
+                alt="CatOps Logo" 
+                class="w-5 h-5 mr-1 rounded-sm object-cover"
+            />
+            CatOps
+            
+        </NuxtLink>
+    </span>
+    <span class="text-sm sm:text-base">
+        Open to exciting projects (DM if you'd like to work together)
+    </span>
+</p>
                 <div class="flex space-x-4 my-4">
                     <a href="https://www.linkedin.com/in/gorislavets/" class="text-zinc-600 hover:text-zinc-400 transition">
                         <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><!-- Icon from Huge Icons by Hugeicons - undefined --><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" color="currentColor"><path d="M7 10v7m4-4v4m0-4a3 3 0 1 1 6 0v4m-6-4v-3M7.008 7h-.009"/><path d="M2.5 12c0-4.478 0-6.718 1.391-8.109S7.521 2.5 12 2.5c4.478 0 6.718 0 8.109 1.391S21.5 7.521 21.5 12c0 4.478 0 6.718-1.391 8.109S16.479 21.5 12 21.5c-4.478 0-6.718 0-8.109-1.391S2.5 16.479 2.5 12"/></g></svg>
@@ -21,6 +37,9 @@
                     </a>
                     <a href="https://instagram.com/milan1337_" class="text-zinc-600 hover:text-zinc-400 transition">
                         <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><!-- Icon from Huge Icons by Hugeicons - undefined --><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" color="currentColor"><path d="M2.5 12c0-4.478 0-6.718 1.391-8.109S7.521 2.5 12 2.5c4.478 0 6.718 0 8.109 1.391S21.5 7.521 21.5 12c0 4.478 0 6.718-1.391 8.109S16.479 21.5 12 21.5c-4.478 0-6.718 0-8.109-1.391S2.5 16.479 2.5 12"/><path d="M16.5 12a4.5 4.5 0 1 1-9 0a4.5 4.5 0 0 1 9 0m1.008-5.5h-.01"/></g></svg>
+                    </a>
+                    <a href="mailto:milan@gorislavets.com" class="text-zinc-600 hover:text-zinc-400 transition">
+                        <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><!-- Icon from Huge Icons by Hugeicons - undefined --><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" color="currentColor"><path d="m5 7l-1.218.813c-.874.582-1.311.873-1.548 1.317c-.237.445-.235.967-.231 2.01c.004 1.258.016 2.538.048 3.834c.077 3.075.115 4.612 1.246 5.742c1.13 1.13 2.688 1.17 5.804 1.248a116 116 0 0 0 5.799 0c3.116-.078 4.674-.117 5.804-1.248c1.13-1.13 1.169-2.667 1.246-5.742c.032-1.296.044-2.576.048-3.833c.003-1.044.005-1.566-.232-2.01c-.236-.445-.673-.736-1.547-1.318L19 7"/><path d="m2 10l6.913 4.148C10.417 15.05 11.169 15.5 12 15.5s1.583-.45 3.087-1.353L22 10"/><path d="M5 12V6c0-1.886 0-2.828.586-3.414S7.114 2 9 2h6c1.886 0 2.828 0 3.414.586S19 4.114 19 6v6m-9-2h4m-4-4h4"/></g></svg>
                     </a>
                     <a href="https://peerlist.io/goris" class="text-zinc-600 hover:text-zinc-400 transition">
                         <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><!-- Icon from Iconoir by Luca Burgio - https://github.com/iconoir-icons/iconoir/blob/main/LICENSE --><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"><path d="M8.87 3h6.26a6 6 0 0 1 5.963 5.337l.21 1.896c.131 1.174.131 2.36 0 3.534l-.21 1.896A6 6 0 0 1 15.13 21H8.87a6 6 0 0 1-5.963-5.337l-.21-1.896a16 16 0 0 1 0-3.534l.21-1.896A6 6 0 0 1 8.87 3"/><path d="M9 17v-4m0 0V7h4a3 3 0 0 1 3 3v0a3 3 0 0 1-3 3z"/></g></svg>
@@ -32,7 +51,11 @@
                         I wrote my first lines of code when I was 11, and now - just 16 and a half years old - Ive already spent nearly six years sharpening my skills, experimenting, and bringing ideas to life.
                 </p>
                 <div class="my-4">
-                    <div class="w-full h-48 rounded-xl border border-gray-200"></div>
+                    <div class="w-full h-48 rounded-xl border border-gray-200">
+                        <div class="p-4 rounded-2xl">
+                            
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
